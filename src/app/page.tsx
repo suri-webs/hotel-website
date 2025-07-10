47b9dafedde46d97444e111957d6d80c68315b94
suraj-web <shakyasuraj595@gamil.com>
2025-07-10 12:56:47 +0530

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
-
-import { ContactUs } from "@/components/contactus/Contact";
-import Rooms from "@/components/rooms/Rooms";
-import Activities from "@/components/activities/Activities";
-import Testmonilas from "@/components/Testmonilas/Testmonilas";
-import Header from "@/components/header/Header";
-import Services from "@/components/Services/Services";
-import Video from "@/components/Vedio/Vedio";
 import About from "@/components/about/About";
+import Activities from "@/components/activities/Activities";
+import { ContactUs } from "@/components/contactus/Contact";
+import Header from "@/components/header/Header";
+import Rooms from "@/components/rooms/Rooms";
+import Services from "@/components/Services/Services";
+import Testmonilas from "@/components/Testmonilas/Testmonilas";
+import Video from "@/components/Vedio/Vedio";
+
 
 
 export default function Home() {
   return (
     <>
-    <div className="overflow-x-hidden">
-      <Header />
-      <About/>
-      <Services />
-      <Rooms />
-      <Video />
-      <Activities />
-      <Testmonilas />
-      <ContactUs />
-    </div>
+      <div className="overflow-x-hidden">
+        <Header />
+        <About />
+        <Services />
+        <Rooms />
+        <Video />
+        <Activities />
+        <Testmonilas />
+        <ContactUs />
+      </div>
 
     </>
 
